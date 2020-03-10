2f10751b876f5703be99524469bcb2f77d81fcac
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-10 17:34:43 -0500

    winner reset

--- scripts/dom.js
+++ scripts/dom.js
@@ -26,6 +26,7 @@ Array.from(gameboard.children).forEach( (box, index) => {
 restart.addEventListener('click', e => {
   game.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   game.flow = 'X';
+  game.winner = null
   Array.from(gameboard.children).forEach( (box, index) => {
     box.classList.remove('box-check-O')
     box.classList.remove('box-check-X')
