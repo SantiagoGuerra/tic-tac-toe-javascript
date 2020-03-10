110b56b2999bb78c74728649529bc16daf581c92
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-10 15:01:49 -0500

    Events implemented

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,1 +1,22 @@
 //dom manipulation logic
+
+const game = GameBoard()
+const gameboard = document.querySelector('.gameboard');
+const restart = document.querySelector('#restart')
+
+Array.from(gameboard.children).forEach( (box, index) => {
+  box.addEventListener('click', e => {
+    box.classList.add('box-check')
+    game.board[index] = 'X'
+    console.log(game.board)
+  })
+})
+
+restart.addEventListener('click', e => {
+  game.board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  Array.from(gameboard.children).forEach( (box, index) => {
+    box.classList.remove('box-check')
+  })
+  console.log(game.board)
+
+})
