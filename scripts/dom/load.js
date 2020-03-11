48df44948f4dc7e54dd9bf73b5a4dc6e281c1cfc
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-03-11 19:56:44 -0300

    Remove unncesary code and bring order to chaos

--- scripts/dom/load.js
+++ scripts/dom/load.js
@@ -1,22 +1,8 @@
-
 const restart = document.querySelector('#restart')
 const new_game = document.querySelector('#new-game')
 const start = document.querySelector("#start-game")
 const gameboard = document.querySelector('.gameboard');
-
+const game = GameBoard()
 
 document.querySelector('#input-player1').value = ""
 document.querySelector('#input-player2').value = ""
-
-const game = GameBoard()
-
-document.addEventListener('load', e =>  {
-  console.log("Load")
-  //clean the player names fields
-  
-  console.dir(player1_display_name)
-  
-  
-  //hide the player name p tags
-  e.preventDefault()
-})
