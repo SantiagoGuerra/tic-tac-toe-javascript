da1680148c3518a8dfeec1b1a8742cd6cf316654
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-03-11 19:47:56 -0300

    Add new attributes to class GameBoard

--- scripts/logic.js
+++ scripts/logic.js
@@ -2,6 +2,9 @@ const GameBoard = () => {
   let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let flow = "X"
   let winner = null
+  let on_course = false
+  let player1_name = null
+  let player2_name = null
 
   const checkWin = (board,player_symbol) => {
     const winArr = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6]];
