d0dade9db33924216a315a0e796c575cc78ea9a9
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-12 12:14:58 -0500

    validate if game is running

--- scripts/dom/gameboard.js
+++ scripts/dom/gameboard.js
@@ -1,24 +1,27 @@
 Array.from(gameboard.children).forEach( (box, index) => {
   box.addEventListener('click', e => {    
-    if ( ! game.on_course ){
-      //Shows 'error'
-    }
+    
     if(typeof game.board[index] === 'number' && game.winner == null && game.on_course){
       box.classList.add('box-check-' + game.flow);
       game.board[index] = game.flow;
-      if (game.checkWin(game.board,game.flow)) {
-        //let winner_name = "Pepito Honguito"
-        //let loser_name = "Juan Perez"
+      let winnerSymbol = game.checkWin(game.board,game.flow)
+      if (winnerSymbol) {
+        let winner_name = (winnerSymbol === 'X') ? game.player1_name : game.player2_name
         game.winner = game.flow
         game.on_course = false
-        console.log('Winner Flow: ' + game.flow)
-        console.log('Winner: ' + game.winner)
-        //alert("Congratulations " + winner_name + ", you won over " + loser_name)
+        showMessage(`Congratulations ${winner_name}  you won`)
       }
       game.flow === 'X'? game.flow = 'O': game.flow = 'X';
       
     }
-    console.log(game.board);
+
+    if(game.board.every(cell => typeof cell === 'string') && !game.winner) {
+      showMessage('Nobody wins')
+    }
+
+    if ( !game.on_course && !game.winner){
+      showMessage('You need to fill name inputs in order to play.')
+    }
 
   })
 })
