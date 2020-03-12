70042b4ebf6c27d91b593cc50b1fbcd6d2e03da3
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-12 12:15:34 -0500

    player names added

--- scripts/dom/load.js
+++ scripts/dom/load.js
@@ -2,7 +2,23 @@ const restart = document.querySelector('#restart')
 const new_game = document.querySelector('#new-game')
 const start = document.querySelector("#start-game")
 const gameboard = document.querySelector('.gameboard');
-const game = GameBoard()
+const game = GameBoard();
+const messages = document.querySelector('.messages');
+let input_player1 = document.querySelector('#input-player1');  
+let input_player2 = document.querySelector('#input-player2');
+const player1_display_name = document.querySelector('#player1-name')
+const player2_display_name = document.querySelector('#player2-name')
+
+const showMessage = message => {
+  messages.innerHTML = `<p>${message}</p>`
+}
+
+showMessage('Please insert player names')
+
+
 
 document.querySelector('#input-player1').value = ""
 document.querySelector('#input-player2').value = ""
+
+
+
