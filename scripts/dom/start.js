1e203732a155664ae9f2a9b2fdaae60aaee31d93
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-12 14:27:23 -0500

    eslint issues

--- scripts/dom/start.js
+++ scripts/dom/start.js
@@ -1,28 +1,26 @@
-start.addEventListener('click', e => {    
-   
+start.addEventListener('click', e => {
+  game.player1_name = (input_player1.value === '') ? 'Anonymous 1' : input_player1.value;
+  input_player1.classList.add('d-none');
 
-  game.player1_name = (input_player1.value === '') ? 'Anonymous 1' : input_player1.value 
-  input_player1.classList.add('d-none')  
+  game.player2_name = (input_player2.value === '') ? 'Anonymous 2' : input_player2.value;
+  input_player2.classList.add('d-none');
 
-  game.player2_name =( input_player2.value === '' ) ? 'Anonymous 2' : input_player2.value
-  input_player2.classList.add('d-none')  
-  
-  // //Hide the message "please insert player names" 
-  showMessage('')
-  
-  start.classList.add('d-none') //hides "Let's start button"
-  
-  //Shows player names in p tag
-  player1_display_name.textContent = game.player1_name  
-  player1_display_name.classList.remove('d-none')
+  // //Hide the message "please insert player names"
+  showMessage('');
 
-  player2_display_name.textContent = game.player2_name  
-  player2_display_name.classList.remove('d-none')
+  start.classList.add('d-none'); // hides "Let's start button"
 
-  
-  new_game.classList.remove('d-none') //shows new game button
-  restart.classList.remove('d-none') //shows restart button
+  // Shows player names in p tag
+  player1_display_name.textContent = game.player1_name;
+  player1_display_name.classList.remove('d-none');
 
-  game.on_course = true
-  e.preventDefault()
- })
+  player2_display_name.textContent = game.player2_name;
+  player2_display_name.classList.remove('d-none');
+
+
+  new_game.classList.remove('d-none'); // shows new game button
+  restart.classList.remove('d-none'); // shows restart button
+
+  game.on_course = true;
+  e.preventDefault();
+});
--- scripts/logic.js
+++ scripts/logic.js
@@ -1,40 +1,42 @@
+/* eslint no-unused-vars: 2 */
+/* global GameBoard */
 const GameBoard = () => {
-  let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let flow = "X"
-  let winner = null
-  let on_course = false
-  let player1_name = null
-  let player2_name = null
+  const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const flow = 'X';
+  const winner = null;
+  const onCourse = false;
+  const player1Name = null;
+  const player2Name = null;
 
-  const checkWin = (board,player_symbol) => {
-    const winArr = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6]];
-    console.log("jugador: " + player_symbol)
+  const checkWin = (board, playerSymbol) => {
+    const winArr = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6]];
     if (winArr.some(
-        (winVal) => {
-          return winVal.every(
-            (winPos) => {            
-              return board[winPos] === player_symbol
-            }
-        )
-      }
-      ) 
-    ){
-      return player_symbol
-    }else{
-      return false
+      (winVal) => winVal.every(
+        (winPos) => board[winPos] === playerSymbol,
+      ),
+    )
+    ) {
+      return playerSymbol;
     }
-  }
+    return false;
+  };
 
-  const startGame = (player1_name, player2_name) => {
-    this.player1_name = player1_name;
-    this.player2_name = player2_name;
-    this.on_course = true
-  }
-
-  
+  const startGame = (player1Name, player2Name) => {
+    this.player1Name = player1Name;
+    this.player2Name = player2Name;
+    this.onCourse = true;
+  };
 
 
   return {
-    board, flow, checkWin, winner, startGame, player2_name, player1_name
-  }
-}
+    board, flow, checkWin, winner, startGame, player2Name, player1Name, onCourse,
+  };
+};
