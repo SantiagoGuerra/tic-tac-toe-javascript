639d858bf1c361ad2e5089052312fc3c39e7c76c
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-12 14:26:29 -0500

    eslint issues

--- scripts/dom/gameboard.js
+++ scripts/dom/gameboard.js
@@ -1,27 +1,24 @@
-Array.from(gameboard.children).forEach( (box, index) => {
-  box.addEventListener('click', e => {    
-    
-    if(typeof game.board[index] === 'number' && game.winner == null && game.on_course){
-      box.classList.add('box-check-' + game.flow);
+Array.from(gameboard.children).forEach((box, index) => {
+  box.addEventListener('click', e => {
+    if (typeof game.board[index] === 'number' && game.winner == null && game.on_course) {
+      box.classList.add(`box-check-${game.flow}`);
       game.board[index] = game.flow;
-      let winnerSymbol = game.checkWin(game.board,game.flow)
+      const winnerSymbol = game.checkWin(game.board, game.flow);
       if (winnerSymbol) {
-        let winner_name = (winnerSymbol === 'X') ? game.player1_name : game.player2_name
-        game.winner = game.flow
-        game.on_course = false
-        showMessage(`Congratulations ${winner_name}  you won`)
+        const winner_name = (winnerSymbol === 'X') ? game.player1_name : game.player2_name;
+        game.winner = game.flow;
+        game.on_course = false;
+        showMessage(`Congratulations ${winner_name}  you won`);
       }
-      game.flow === 'X'? game.flow = 'O': game.flow = 'X';
-      
+      game.flow === 'X' ? game.flow = 'O' : game.flow = 'X';
     }
 
-    if(game.board.every(cell => typeof cell === 'string') && !game.winner) {
-      showMessage('Nobody wins')
+    if (game.board.every(cell => typeof cell === 'string') && !game.winner) {
+      showMessage('Nobody wins');
     }
 
-    if ( !game.on_course && !game.winner){
-      showMessage('You need to fill name inputs in order to play.')
+    if (!game.on_course && !game.winner) {
+      showMessage('You need to fill name inputs in order to play.');
     }
-
-  })
-})
+  });
+});
--- scripts/dom/new-game.js
+++ scripts/dom/new-game.js
@@ -1,26 +1,26 @@
 new_game.addEventListener('click', e => {
-  restart.dispatchEvent(new Event('click'))
-  game.on_course = false
+  restart.dispatchEvent(new Event('click'));
+  game.on_course = false;
 
-  showMessage('Insert player names')
-  restart.classList.add('d-none')
-  new_game.classList.add('d-none')
-  start.classList.remove('d-none')
+  showMessage('Insert player names');
+  restart.classList.add('d-none');
+  new_game.classList.add('d-none');
+  start.classList.remove('d-none');
 
-  player1_display_name.classList.add('d-none')
-  player2_display_name.classList.add('d-none')
-
-
-  input_player1.classList.remove('d-none')
-  input_player1.value = ""
-  game.player1_name = ''
-
-  input_player2.classList.remove('d-none')
-  input_player2.value = ""
-  game.player2_name = ''
+  player1_display_name.classList.add('d-none');
+  player2_display_name.classList.add('d-none');
 
 
-  //erase player names on the gameboard
-  //erase player names on the dom -> objects
-  //show text input
-})
+  input_player1.classList.remove('d-none');
+  input_player1.value = '';
+  game.player1_name = '';
+
+  input_player2.classList.remove('d-none');
+  input_player2.value = '';
+  game.player2_name = '';
+
+
+  // erase player names on the gameboard
+  // erase player names on the dom -> objects
+  // show text input
+});
