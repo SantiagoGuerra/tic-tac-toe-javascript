a8979ee59d4b9f130d6c8b0e0c97d0cabcf60216
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-21 14:17:40 -0500

    Module selectors file

--- src/dom/selectors.js
+++ src/dom/selectors.js
@@ -1,11 +1,9 @@
-/* eslint no-unused-vars: */
-
-const restart = document.querySelector('#restart');
-const newGame = document.querySelector('#new-game');
-const start = document.querySelector('#start-game');
-const gameboardDOM = document.querySelector('.gameboard');
-const messages = document.querySelector('.messages');
-const inputPlayer1 = document.querySelector('#input-player1');
-const inputPlayer2 = document.querySelector('#input-player2');
-const player1DisplayName = document.querySelector('#player1-name');
-const player2DisplayName = document.querySelector('#player2-name');
+export const restart = document.querySelector('#restart');
+export const newGame = document.querySelector('#new-game');
+export const start = document.querySelector('#start-game');
+export const gameboardDOM = document.querySelector('.gameboard');
+export const messages = document.querySelector('.messages');
+export const inputPlayer1 = document.querySelector('#input-player1');
+export const inputPlayer2 = document.querySelector('#input-player2');
+export const player1DisplayName = document.querySelector('#player1-name');
+export const player2DisplayName = document.querySelector('#player2-name');
